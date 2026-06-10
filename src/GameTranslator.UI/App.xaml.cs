194442d0e59c3eb4b82548cc9b6f970f8dd0cf7c
Dementius-cell <291898246+Dementius-cell@users.noreply.github.com>
2026-06-10 21:46:50 +0300

    configure sprint 0 host and logging

--- src/GameTranslator.UI/App.xaml.cs
+++ src/GameTranslator.UI/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace GameTranslator.UI;
 
@@ -9,5 +11,54 @@ namespace GameTranslator.UI;
 /// </summary>
 public partial class App : System.Windows.Application
 {
+    private readonly IHost host;
+
+    public App()
+    {
+        host = Host.CreateDefaultBuilder()
+            .UseSerilog((context, services, loggerConfiguration) =>
+            {
+                loggerConfiguration
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Debug()
+                    .WriteTo.File(
+                        path: "logs/game_translator_.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 14);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        host.StartAsync().GetAwaiter().GetResult();
+        Log.Information("GameTranslator application started.");
+
+        MainWindow = host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        try
+        {
+            Log.Information("GameTranslator application stopped.");
+            host.StopAsync(TimeSpan.FromSeconds(5)).GetAwaiter().GetResult();
+        }
+        finally
+        {
+            host.Dispose();
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
+    }
 }
 
